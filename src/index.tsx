75e92564304000074a6d88ab498efd0561042062
artmzvn@gmail.com <artmzvn@gmail.com>
2022-04-15 00:11:15 +0300

    Router fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import { Route, Switch } from 'react-router';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from '@/App';
@@ -10,11 +8,7 @@ import '@/assets/styles/site.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-      <Switch>
-        <Route path={'/'} component={App} />
-      </Switch>
-    </HashRouter>
+      <App />
   </Provider>,
   document.getElementById('app'),
 );
